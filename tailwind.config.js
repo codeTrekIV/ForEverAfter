e5d7a8c75daaecce8f47f31d3218bcb097b9fd37
Louis Filip <louisfilip@gmail.com>
2024-04-22 12:53:19 -0500

    Initial Testing Branch

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    ".src/app/pages/**/*.{js,ts,jsx,tsx}", // paths to all files that contain Tailwind's classes
+    ".src/app/components/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
